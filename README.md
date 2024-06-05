0e741297c5d9ba3ba83e27c64fc7971cd64aa9e7
kalaiselvan30 <kalaiselvan8521@gmail.com>
2024-06-05 21:06:39 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PDF_maker
+# PDF_maker
---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <form action="" method="post">
+        <label className="" htmlFor="input-data">Input data</label>
+        <textarea name="input-data" id=""></textarea>
+        <button type="submit">Convert</button>
+        <button>Download</button>
+      </form>
+    </>
+  )
+}
+
+export default App
